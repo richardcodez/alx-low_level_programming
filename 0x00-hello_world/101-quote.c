6c8e447ea7ca09689972e2c1fcf6ecfd53c8b7be
Richard <lebotsa.cr@gmail.com>
2023-03-16 16:36:29 -0700

    task 18 resubmit again

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,17 +3,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	char string[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	int n;
-	int stringlength = strlen(string);
-
-	for (n = 0; n++; n < stringlength)
-	{
-		putchar("%c",string[n]);
-	}
-	return (0);
+	fput("and that piece of art is useful \" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
 }

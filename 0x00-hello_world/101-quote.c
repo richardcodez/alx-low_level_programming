fcf5c1ce9a321b44cd2128c8a9d1c80dab300839
Richard <lebotsa.cr@gmail.com>
2023-03-16 16:00:32 -0700

    task 8 C hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char string = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	for (int n = 0, n++, n < strlen(string))
+	{
+		putchar(string[n]);
+	}
+	return (0);
+}

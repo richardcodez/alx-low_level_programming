367d4c675ae215599f60a02d9607eb08addd7054
Richard <lebotsa.cr@gmail.com>
2023-03-17 12:34:46 -0700

    task 2 C programming project 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * program for printing alphates in lowercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}

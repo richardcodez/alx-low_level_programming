c69138e119697dc093f62314bf7d800726747303
Richard <lebotsa.cr@gmail.com>
2023-03-21 07:06:16 -0700

    task 4 submit again

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _islower - entry point
+ * _isalpha - entry point
  * @c: the parameter for inputing letter to test
  * Return: 1 if lowercase or 0 if not
  */
 
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c>= 'A' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);

3d99af8348202ba5afa7087a95aca53adcd3d00f
Richard <lebotsa.cr@gmail.com>
2023-03-21 05:09:37 -0700

    task 1 renaming files

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,11 +2,15 @@
 
 /**
  * main - check the code
- *
+ * function that prints alphabet in lowercase
  * Return: Always 0
  */
-int main(void)
+
+void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
 }
--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -2,15 +2,11 @@
 
 /**
  * main - check the code
- * function that prints alphabet in lowercase
+ *
  * Return: Always 0
  */
-
-void print_alphabet(void)
+int main(void)
 {
-	char letter;
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
-	_putchar('\n');
+	print_alphabet();
+	return (0);
 }

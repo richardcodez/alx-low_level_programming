761cfded9baac695d0e5959ce1b2f2249f885ec7
Richard <lebotsa.cr@gmail.com>
2023-03-21 06:44:44 -0700

    task 4 function for checking if is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - entry point
+ * @c: the parameter for inputing letter to test
+ * Return: 1 if lowercase or 0 if not
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c>= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+	_putchar('\n');
+}

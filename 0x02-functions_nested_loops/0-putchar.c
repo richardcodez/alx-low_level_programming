50b5c72bc05a66a7e32c871a13b5a5ad2a9045cb
Richard <lebotsa.cr@gmail.com>
2023-03-21 02:49:43 -0700

    task 0 of my 3rd C project, functions and nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for th program
+ * a program that prints _putchar followed by a new line
+ * Return: 0 for success
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}

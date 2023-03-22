72242661b40306937ded49566bbe0ac283d06fff
Richard <lebotsa.cr@gmail.com>
2023-03-21 21:09:21 -0700

    task 11 print 0 to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - entry point
+ * @n: integer input to function
+ * Return: returnsnothing
+ */
+
+void print_to_98(int n)
+{
+	for (n = 0; n < 99; n++)
+		printf("%d, ", n);
+}

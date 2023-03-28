771ae5cf53aef6e403ee291cbd4a62641d8ad6da
Richard <lebotsa.cr@gmail.com>
2023-03-27 23:03:22 -0700

    task 0 reset_to-98.c content

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to)98 - function that updates the value of a pointer to 98
+ * @n: function input
+ * Return - nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

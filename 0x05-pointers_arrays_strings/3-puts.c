e897ce1b4ed016d80d90d3fe3c0ee78995e5c207
Richard <lebotsa.cr@gmail.com>
2023-03-28 07:48:53 -0700

    task 3 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string
+ * followed by a new line
+ * @str: input string
+ * Return: returns nothing
+ */
+
+void _puts(char *str)
+{
+	int x = 0;
+
+	while (*sstr)
+	{
+		_putchar(*str++);
+		x++;
+	}
+
+	_putchar('\n');
+}

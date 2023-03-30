ec9e4fc1d53b354032c7d5682e74fb0875ee05e1
Richard <lebotsa.cr@gmail.com>
2023-03-30 08:49:30 -0700

    task 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - function that prints n elements
+ * of an array, separated by a comma
+ * @n: input 2
+ * @a: inpu 1
+ * Returns: nothing
+ */
+
+void puts_array(int *a, int n)
+{
+	int i;
+	for (i = 0; i < (n - 1); i++)
+		printf("%d, " a[1]);
+	if (i == (n - 1))
+		printf("%d", a[n - 1]);
+
+	_putchar('\n');
+}

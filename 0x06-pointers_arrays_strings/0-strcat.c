8965f9b4d172700a2feb0bdcd3d2eae9fedd0a49
Richard <lebotsa.cr@gmail.com>
2023-03-31 08:49:41 -0700

    task 0-strcat.c last

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,13 +10,18 @@
 
 char *strcat(char *dest, const char *src)
 {
-	int index = 0, dest_len = 0;
+	int i = 0, j = 0;
 
-	while (dest[index++])
-		dest_len++;
+	while (dest[i] != '\0')
+		i++;
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len] = src[index];
+	while (src[i] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
 
 	return (dest);
 }

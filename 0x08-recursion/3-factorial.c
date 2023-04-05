c6dc95118b2830b88577933262791b9f187e1de4
Richard <lebotsa.cr@gmail.com>
2023-04-05 16:45:24 -0700

    task 3 0x08-recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,11 +6,11 @@
  * Return: racotial of n
  */
 
-int factorial (int n)
+int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
-	else if ( n == 0)
+	else if (n == 0)
 		return (1);
 
 	return (n * factorial(n - 1));

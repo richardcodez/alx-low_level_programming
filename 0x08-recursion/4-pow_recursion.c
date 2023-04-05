4b92cab074ea287273411018b7c7785e101893fc
Richard <lebotsa.cr@gmail.com>
2023-04-05 16:50:21 -0700

    task 4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the value of x^y
+ * @x: input 1
+ * @y: input 2
+ * Return: x^y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
+}
